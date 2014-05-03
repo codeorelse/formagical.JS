7b66598a5e4065af0e2cc24917b320502450099a
Siebe Wiebe Hiemstra <siepprivate@gmail.com>
2014-05-03 13:50:30 +0200

    Add some tests

--- js/formagical.js
+++ js/formagical.js
@@ -14,6 +14,9 @@
       this.getSetting = function(key) {
         return this.settings[key];
       };
+      this.trackInAnalytics = function() {
+
+      };
       this.callSettingFunction = function(name, args) {
         if (args == null) {
           args = [];
@@ -21,7 +24,32 @@
         return this.settings[name].apply(this, args);
       };
       this.init = function() {
+
         this.settings = $.extend({}, this.defaults, options);
+        var that = this;
+
+
+
+        this.$element.find('input, textarea').each(function() {
+          var elementName = $(this).attr('name');
+
+          // Every item should have name
+          if(typeof elementName == 'undefined' || elementName == '') {
+            throw new Error('Every form element should have a name')
+          }
+
+          $(this).blur(function() {
+            if(that.settings.track)
+              that.settings.track.call(that, $(this).data('startFocus'));
+          })
+
+          $(this).focus(function() {
+            if(that.settings.track)
+              that.settings.track.apply(that);
+          })
+
+        })
+
         return this.setState('ready');
       };
       this.init();
--- spec/javascripts/formagicSpec.js
+++ spec/javascripts/formagicSpec.js
@@ -8,24 +8,62 @@
       loadFixtures('fragment.html');
       return this.$element = $('#fixtures');
     });
-    describe('plugin behavior', function() {
+    describe('plugin', function() {
       it('should be available on the jQuery object', function() {
         return expect($.fn.formagical).toBeDefined();
       });
       it('should be chainable', function() {
         return expect(this.$element.formagical()).toBe(this.$element);
       });
+
+      it('should throw error if item in form has no name', function() {
+        loadFixtures('no_name.html');
+        this.$element = $('#no-name');
+        var that = this;
+        return expect(function() {$.formagical(that.$element);}).toThrow();
+      });
+
       it('should offers default values', function() {
         var plugin;
         plugin = new $.formagical(this.$element);
         return expect(plugin.defaults).toBeDefined();
       });
+
       return it('should overwrites the settings', function() {
         var plugin;
         plugin = new $.formagical(this.$element, options);
         return expect(plugin.settings.message).toBe(options.message);
       });
     });
+
+    describe("inputs", function() {
+      it("should call track on focus out", function() {
+        options.track = function(form, formElementName, trackType, trackData) {};
+        var spy = spyOn(options,'track');
+        var plugin;
+        plugin = new $.formagical(this.$element, options);
+
+        $('input',this.$element).eq(0).data('startFocus',100)
+
+        $('input',this.$element).eq(0).trigger('blur');
+
+        return expect(spy).toHaveBeenCalledWith(100);
+      });
+    });
+
+    describe("form", function() {
+      it("should call track when user leaves page", function() {
+        options.track = function(form, formElementName, trackType, trackData) {};
+        var spy = spyOn(options,'track');
+        var plugin;
+        plugin = new $.formagical(this.$element, options);
+
+        $(window).trigger('beforeunload');
+
+        // return expect(spy).toHaveBeenCalled();
+      });
+    });
+
     return describe('plugin state', function() {
       beforeEach(function() {
         return this.plugin = new $.formagical(this.$element);
